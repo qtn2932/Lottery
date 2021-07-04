34a9529b7bc385b23ba3ef09f9e3822dbeacbdef
Quan Nguyen <quan.nguyen199712@gmail.com>
2021-07-03 23:38:52 -0400

    added enter test

---
+++ test/Lottery.test.js
@@ -0,0 +1,63 @@
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3');
+const web3 = new Web3(ganache.provider());
+
+const {interface, bytecode}= require('../compile');
+
+let lottery;
+let accounts;
+
+beforeEach(async ()=>{
+    accounts = await web3.eth.getAccounts();
+
+    lottery= await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({data: bytecode})
+        .send({ from: accounts[0], gas: 1000000})
+});
+
+describe('Loterry Contract', ()=>{
+    it('deployed a contract', ()=>{
+        assert.ok(lottery.options.address);
+    });
+
+    it('allow one player to enter', async ()=>{
+        await lottery.methods.enter().send({
+            from: accounts[0],
+            value: web3.utils.toWei('0.02', 'ether')
+        });
+
+        const players = await lottery.methods.getPlayers().call({
+            from: accounts[0]
+        });
+        assert.strictEqual(accounts[0], players[0]);
+        assert.strictEqual(1, players.length);
+    })
+
+    it('allow multiple players to enter', async ()=>{
+        await lottery.methods.enter().send({
+            from: accounts[0],
+            value: web3.utils.toWei('0.02', 'ether')
+        });
+
+
+        await lottery.methods.enter().send({
+            from: accounts[1],
+            value: web3.utils.toWei('0.02', 'ether')
+        });
+
+
+        await lottery.methods.enter().send({
+            from: accounts[2],
+            value: web3.utils.toWei('0.02', 'ether')
+        });
+
+        const players = await lottery.methods.getPlayers().call({
+            from: accounts[0]
+        });
+        assert.strictEqual(accounts[0], players[0]);
+        assert.strictEqual(accounts[1], players[1]);
+        assert.strictEqual(accounts[2], players[2]);
+        assert.strictEqual(3, players.length);
+    })
+});
